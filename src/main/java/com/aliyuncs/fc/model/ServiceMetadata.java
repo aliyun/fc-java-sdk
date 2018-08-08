f6d23083860f39b774e7c8352412581e96b03071
Ryan Zhang <yzhang8@gmail.com>
2018-08-07 22:55:57 -0700

    add nas support (#43)
    add nas support

--- src/main/java/com/aliyuncs/fc/model/ServiceMetadata.java
+++ src/main/java/com/aliyuncs/fc/model/ServiceMetadata.java
@@ -11,12 +11,14 @@ public class ServiceMetadata {
     private LogConfig logConfig;
     private VpcConfig vpcConfig;
     private Boolean internetAccess;
+    private NasConfig nasConfig;
     private String serviceId;
     private String createdTime;
     private String lastModifiedTime;
 
     public ServiceMetadata(String serviceName, String description, String role,
-                           LogConfig logConfig, VpcConfig vpcConfig, Boolean internetAccess, String serviceId, String createdTime, String lastModifiedTime) {
+        LogConfig logConfig, VpcConfig vpcConfig, Boolean internetAccess, NasConfig nasConfig,
+        String serviceId, String createdTime, String lastModifiedTime) {
         this.serviceName = serviceName;
         this.description = description;
         this.role = role;
@@ -26,6 +28,7 @@ public class ServiceMetadata {
         this.lastModifiedTime = lastModifiedTime;
         this.vpcConfig = vpcConfig;
         this.internetAccess = internetAccess;
+        this.nasConfig = nasConfig;
     }
 
     public String getServiceName() {
@@ -62,5 +65,9 @@ public class ServiceMetadata {
 
     public VpcConfig getVpcConfig() {
         return vpcConfig;
+    }
+
+    public NasConfig getNasConfig() {
+        return nasConfig;
     }
 }
