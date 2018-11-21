b712ff953f73fb67ccb25d39a4c29bc4ce3afd3f
jhstianwei <1058211924@qq.com>
2018-11-21 13:47:34 +0800

    Get account settings input (#58)
    * refactor test list trigger code
    
    * add rds support
    
    * modify RdsTriggerConfig structure
    
    * add initializer interface for java sdk
    
    * get account setting input
    
    * pretty import
    
    * fix bugger
    
    * rebase master to update code
    
    * fix bugger
    
    * fix mr issue
    
    * delete account setting model set function

---
+++ src/main/java/com/aliyuncs/fc/model/AccountSettings.java
@@ -0,0 +1,13 @@
+package com.aliyuncs.fc.model;
+
+/**
+ * Created by tw108174 on 18/10/30.
+ */
+public class AccountSettings {
+
+    private String[] availableAZs;
+
+    public String[] getAvailableAZs() {
+        return availableAZs;
+    }
+}
