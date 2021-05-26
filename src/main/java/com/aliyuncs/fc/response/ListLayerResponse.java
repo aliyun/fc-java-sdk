1ad4745e608714fdf228d6b3ac77101c5929e46f
ehtan.zcl <ethan.zcl@alibaba-inc.com>
2021-05-26 16:56:11 +0800

    update layer api

--- src/main/java/com/aliyuncs/fc/response/ListLayerResponse.java
+++ src/main/java/com/aliyuncs/fc/response/ListLayerResponse.java
@@ -1,105 +1,15 @@
 package com.aliyuncs.fc.response;
 
 import com.aliyuncs.fc.http.HttpResponse;
+import com.aliyuncs.fc.model.Layer;
+
+import java.util.Arrays;
 
 public class ListLayerResponse extends HttpResponse {
 
     private Layer[] layers;
 
     private String nextToken;
-
-    class Layer {
-        private String layerName;
-        private String version;
-        private String description;
-        private Object code;
-        private Integer codeSize;
-        private String codeChecksum;
-        private String createTime;
-        private String[] compatibleRuntime;
-        private Integer acl;
-        private String Arn;
-
-        public String getLayerName() {
-            return layerName;
-        }
-
-        public void setLayerName(String layerName) {
-            this.layerName = layerName;
-        }
-
-        public String getVersion() {
-            return version;
-        }
-
-        public void setVersion(String version) {
-            this.version = version;
-        }
-
-        public String getDescription() {
-            return description;
-        }
-
-        public void setDescription(String description) {
-            this.description = description;
-        }
-
-        public Object getCode() {
-            return code;
-        }
-
-        public void setCode(Object code) {
-            this.code = code;
-        }
-
-        public Integer getCodeSize() {
-            return codeSize;
-        }
-
-        public void setCodeSize(Integer codeSize) {
-            this.codeSize = codeSize;
-        }
-
-        public String getCodeChecksum() {
-            return codeChecksum;
-        }
-
-        public void setCodeChecksum(String codeChecksum) {
-            this.codeChecksum = codeChecksum;
-        }
-
-        public String getCreateTime() {
-            return createTime;
-        }
-
-        public void setCreateTime(String createTime) {
-            this.createTime = createTime;
-        }
-
-        public String[] getCompatibleRuntime() {
-            return compatibleRuntime;
-        }
-
-        public void setCompatibleRuntime(String[] compatibleRuntime) {
-            this.compatibleRuntime = compatibleRuntime;
-        }
-
-        public Integer getAcl() {
-            return acl;
-        }
-
-        public void setAcl(Integer acl) {
-            this.acl = acl;
-        }
-
-        public String getArn() {
-            return Arn;
-        }
-
-        public void setArn(String arn) {
-            this.Arn = arn;
-        }
-    }
 
     public Layer[] getLayers() {
         return layers;
@@ -115,5 +25,13 @@ public class ListLayerResponse extends HttpResponse {
 
     public void setNextToken(String nextToken) {
         this.nextToken = nextToken;
+    }
+
+    @Override
+    public String toString() {
+        return "ListLayerResponse{" +
+                "layers=" + Arrays.toString(layers) +
+                ", nextToken='" + nextToken + '\'' +
+                '}';
     }
 }
